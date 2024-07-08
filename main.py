47720333edfab750e8fe307e81495a8561661bfe
davarmenia <david.gharagyozyan17@gmail.com>
2024-07-08 18:53:01 +0400

    Animation for speed numbers.

--- main.py
+++ main.py
@@ -20,14 +20,26 @@ pygame.display.set_caption('Dashboard')
 GAME_CLOCK = pygame.time.Clock()
 
 class InfoText():
+    def __init__(self):
+        self.color_rgb = [1,1,1,1,1,1,1,1]
+        self.color_ind_to_change = -1
+        self.speed_values = ["30, 60, 90, 120, 150, 180, 210, 240"]
+        self.speed_values_position = [[100,100], [100,120], [100,140], [100,160], [100,180], [100,200], [100,220], [100,240]]
+
     def draw_numbers(self):
         font = pygame.font.Font('AntonSC-Regular.ttf', 22)
-        text_for_show = "%d" % (30)
-        text = font.render(text_for_show, True, WHITE)
-        textRect = text.get_rect()
-        textRect.left = (100)
-        textRect.top = (100)
-        screen.blit(text, textRect)
+        for id, value in enumerate(self.speed_values):
+            text_for_show = "%s" % (value)
+            text = font.render(text_for_show, True, (self.color_rgb[id], self.color_rgb[id], self.color_rgb[id]))
+            if id > self.color_ind_to_change:
+                if self.color_rgb[id] < 200:
+                    self.color_rgb[id] += 3
+                    if self.color_rgb[self.color_ind_to_change] > 100:
+                        self.color_ind_to_change += 1
+            textRect = text.get_rect()
+            textRect.left = (self.speed_values_position[id][0])
+            textRect.top = (self.speed_values_position[id][1])
+            screen.blit(text, textRect)
 
 
 info_text = InfoText()
